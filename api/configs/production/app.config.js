b14d52cd2007cb3f58b1f480004f455e47228c77
Maxime <97433426+Maximelebonm@users.noreply.github.com>
2022-12-12 11:57:19 +0100

    Update app.config.js

--- api/configs/production/app.config.js
+++ api/configs/production/app.config.js
@@ -1,7 +1,7 @@
 const config = {
   PORT: 8000,
   HASH_PREFIX: "$2b$08$",
-  JWT_SECRET: "b3715d5693ef63a7f77454f671a3270a",
+  JWT_SECRET: "",
   RESTRICTED_ROUTES : {
     "/profil": (user) => user.role === 1,
     "/admin": (user) => user.role === 2,
