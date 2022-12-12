ae52ceb89008e8c3e57fb3b2b5437e2dafec3fb9
Maxime <97433426+Maximelebonm@users.noreply.github.com>
2022-12-12 11:56:47 +0100

    Update db.config.js

--- api/configs/production/db.config.js
+++ api/configs/production/db.config.js
@@ -1,9 +1,5 @@
 const config = {
-  HOST: "web50.lws-hosting.com",
-  PORT: "3306",
-  USER: "c1945559c@localhost",
-  PASS: "Rtyubxw304",
-  NAME: "c1945559c_bemusician",
+
 };
 
 module.exports = config;
